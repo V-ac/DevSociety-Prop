e681638a8b9f4052fbf7b56a4f6c05ea7d40a46f
Ver-Ac <veritho1555@gmail.com>
2024-07-17 20:41:10 -0600

    Puntos relacioandos al lenguaje Python

---
+++ Primer_ejercicio.py
@@ -0,0 +1,74 @@
+"""
+Esta parte la decidí ponerla en un mismo programa, ya que son puntos relacionados al lenguaje, sus propiedades y caracteristicas
+
+ Crea un repositorio en GitHub, en este repositorio guardarás todos tus ejercicios
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado. -
+ * - Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...). -
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+
+ * - Crea ejemplos utilizando todos los tipos de operadores de tu lenguaje:
+ 	*   Aritméticos, lógicos, de comparación, asignación, identidad, pertenencia, bits...
+ 	*   (Ten en cuenta que cada lenguaje puede poseer unos diferentes)
+ * - Utilizando las operaciones con operadores que tú quieras, crea ejemplos que representen todos los tipos de estructuras de control que existan en tu lenguaje:
+	 *   Condicionales, iterativas, excepciones...
+	 * - Debes hacer print por consola del resultado de todos los ejemplos.
+
+"""
+
+# TODO 1er punto - El repositorio ya se creó.
+
+# TODO 2do punto - URL del sitio web de Python:  https://www.python.org/
+
+# TODO 3ro punto - :
+
+#La forma más común empieza con el signo "#" como este comentario
+
+#Otra forma:
+""" Esta forma hace que funcione como comentario. Ya que la cadena
+multilínea no se asigna a una variable. En Python se pueden definir cadenas de varialíneas
+encerrandolas entre comillas dobles triples (como este comentario)"""  
+
+'''o con comillas simples triples (como este comentario)'''
+
+# TODO este comentario con la palabra "TODO" hace que sea un comentario resaltado en comparación al que solo tiene el signo gato. Algunos IDE pueden resaltar la palabra.
+
+
+# TODO 4to punto - Crear una variable:
+
+nombre = "Verónica Acuña" #Variable string en python válida
+#En el lenguaje Python no existe una sintaxis que haga referencia a una variable constante.
+#Existen sintaxis que no acepta pyton en las variables:
+"""
+    1numero    -> no empezar con numeros
+    nombre completo  -> no espacios
+    $Nombre  -> no empezar con caracteres especiales.
+
+"""
+
+
+# TODO 5to punto - Datos primitivos del lenguaje:
+
+#numericos
+entero = 10 #int
+flotante = 10.5 #float
+
+#boleano
+verdadero = True
+falso = False
+
+#cadena
+comillasSimples = 'Casa'
+comillasDobles = "Casa"
+cadena = """Casa"""
+cadena2 = '''Casa'''
+
+#none
+declaracion = None;
+
+
+# TODO 6to punto - Imprime por terminal:
+
+print ("¡Hola, Python!")
+
