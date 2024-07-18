7b051689de3e902ce254f20e090b4afc573fbfc3
Ver-Ac <veritho1555@gmail.com>
2024-07-17 21:17:24 -0600

    Agrego el comentario de la actividad de los tipos de operadores

---
+++ tiposDeOperadores.py
@@ -0,0 +1,10 @@
+"""
+* - Crea ejemplos utilizando todos los tipos de operadores de tu lenguaje:
+ 	*   Aritméticos, lógicos, de comparación, asignación, identidad, pertenencia, bits...
+ 	*   (Ten en cuenta que cada lenguaje puede poseer unos diferentes)
+ * - Utilizando las operaciones con operadores que tú quieras, crea ejemplos que representen todos los tipos de estructuras de control que existan en tu lenguaje:
+	 *   Condicionales, iterativas, excepciones...
+	 * - Debes hacer print por consola del resultado de todos los ejemplos.
+"""
+
+
