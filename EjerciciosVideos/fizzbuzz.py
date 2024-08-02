4724df390a1d8da1f07a63ae2866b8c0fd6daddf
V-ac <veritho1555@gmail.com>
2024-08-02 12:51:24 -0600

    Ejercicio terminado

--- EjerciciosVideos/fizzbuzz.py
+++ EjerciciosVideos/fizzbuzz.py
@@ -5,9 +5,30 @@ Si el número es divisible entre 5 imprimimos Buzz
 Si el número es divisible entre 3 y 5 imprimimos FizzBuzz
 """
 
-
+#Esta es la primera forma que se puede solucionar
 def fizzbuz(number):
     for i in range (1, number+1):
-        print(i)
+        #Va primero esta condición para que no se descarte
+        #desde un principio cuando cumpla la condición.
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
 
-fizzbuz(10)
+#Como segunda solución:
+def fizzbuzz2(number):
+    for i in range (1, number+1):
+        #Se va a imprimer Fizz si es divisible entre 3
+        fizz = "Fizz" if i % 3 == 0 else ""
+        #Se va a imprimir Buzz si es divisible entre 5
+        buzz = "Buzz" if i % 5 == 0 else ""
+        #En este caso, si cumple las 2, entonces se va a
+        #imrimir "FizzBuzz"
+        print(f"{fizz}{buzz}" or i)
+
+
+fizzbuzz2(30)
