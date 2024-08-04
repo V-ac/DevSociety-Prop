59e571f78472fbdb8760f74b233227d66eb12cf0
V-ac <veritho1555@gmail.com>
2024-08-04 13:18:15 -0600

    Ejercicio incompleto para solucionar en python

---
+++ EjerciciosVideos/nReinas.py
@@ -0,0 +1,14 @@
+
+def QueenPosition(rowPosition, columnPosition):
+    rowIndex = rowPosition
+    columnIndex = columnPosition
+
+    leftDiagonal = rowIndex + columnIndex
+    rigthDiagonal = rowIndex + columnIndex
+
+
+class QueenPosition:
+    def _init_(rowIndex, columnIndex):
+        row = rowIndex
+        column = columnIndex
+
