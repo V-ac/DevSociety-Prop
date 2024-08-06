305165bde0585db68ff3266315e9633ca991fd57
V-ac <veritho1555@gmail.com>
2024-08-06 17:56:11 -0600

    Ultimo ejercicio de los videos de la lista Algoritmo en Js, en estos días paso este ejercicio en python

---
+++ EjerciciosVideos/ejercicioCesar.js
@@ -0,0 +1,42 @@
+const englishAlphabet = 'abcdefghijklmnopqrstuvxyz'.split('')
+
+const getClipherMap = (alaphabet, shift) => {
+    return alaphabet.
+    reduce((charsMap, currentChar, charIndex) =>{
+        const charsMapsClone = {...charsMap}
+
+        let encrypytedCharIndex = (charIndex + shift) % alaphabet.length
+
+        if(encrypytedCharIndex < 0){
+            encrypytedCharIndex += alaphabet.length
+        }
+
+        charsMapsClone[currentChar] = alaphabet[encrypytedCharIndex]
+
+        return charsMapsClone
+
+    }, {})
+}
+
+const encrypt = (str, shift, alphabet = englishAlphabet) => {
+    const cipherMap = getClipherMap(alphabet, shift)
+
+    return str.toLowerCase.split('').map(char => cipherMap[char] || char).join('')
+}
+
+const dencrypt = (str, shift, alphabet = englishAlphabet) => {
+    const cipherMap = getClipherMap(alphabet, -shift)
+
+    return str.toLowerCase.split('').map(char => cipherMap[char] || char).join('')
+}
+
+const str = 'Hola mundo'
+const enc = encrypt(str, 2)
+const dec = dencrypt(str, 2)
+
+
+
+
+
+
+
